a114187b96783547f7561c7c3c62ed06ed5a7169
Dae Sung Choi <daeseong.choe@gmail.com>
2019-09-25 12:49:49 +0900

    docs: fix the errata in the comment
    fix the errata in the comment
    
    fix the errata in the comment about the changed learning rate after
    525 epoch -> 526 epoch

--- test09_rtm_nn_latL_tozall_train_radlog.py
+++ test09_rtm_nn_latL_tozall_train_radlog.py
@@ -46,7 +46,7 @@ if __name__ == '__main__':
     model = MLPv01_6_800()
     print(model)
     lr = 0.000001 # changed from 0.0001 after 223 epoch
-# changed from 0.00001 after 525 epoch
+# changed from 0.00001 after 526 epoch
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
 
 #loss_fn = nn.CrossEntropyLoss()
